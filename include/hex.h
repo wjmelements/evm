4dbd6d1f454b512447818c6911167eb17492be5a
William Morriss <wjmelements@gmail.com>
2020-12-08 12:44:27 -0800

    compatible hex.h

--- include/hex.h
+++ include/hex.h
@@ -26,11 +26,15 @@ HEX(8) \
 HEX(9) \
 
 static inline uint8_t hexString8ToUint8(const uint8_t hexString8) {
-    switch (hexString8) {
-        #define HEX(c) case #c[0]: return 0x##c;
-        HEXCHARS
-        #undef HEX
-    }
+	if ('0' <= hexString8 && hexString8 <= '9') {
+		return hexString8 - '0';
+	}
+	if ('a' <= hexString8 && hexString8 <= 'f') {
+		return hexString8 - 'a';
+	}
+	if ('A' <= hexString8 && hexString8 <= 'F') {
+		return hexString8 - 'A';
+	}
     fputs("hexString8ToUint8: unexpected hex string", stderr);
     fputc(hexString8, stderr);
     fputc('\n', stderr);
@@ -40,11 +44,14 @@ static inline uint8_t hexString16ToUint8(const char *hexString16) {
     return hexString8ToUint8(hexString16[0]) * 16 + hexString8ToUint8(hexString16[1]);
 }
 static inline int isHex(char h) {
-    switch (h) {
-        #define HEX(c) case #c[0]:
-        HEXCHARS
-        #undef HEX
-        return 1;
-    }
-    return 0;
+	if ('0' <= h && h <= '9') {
+		return 1;
+	}
+	if ('a' <= h && h <= 'f') {
+		return 1;
+	}
+	if ('A' <= h && h <= 'F') {
+		return 1;
+	}
+	return 0;
 }
