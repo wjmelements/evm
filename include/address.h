c0039fa16b454c47416f3c2bef0a5133a06f2f47
William Morriss <wjmelements@gmail.com>
2023-11-13 14:41:18 -0600

    fix AddressEqual

--- include/address.h
+++ include/address.h
@@ -4,7 +4,7 @@ typedef char address_t[20];
 
 static inline int AddressEqual(const address_t expected, const address_t actual) {
     for (int i = 0; i < 20; i ++) {
-        if (expected[i] == actual[i]) {
+        if (expected[i] != actual[i]) {
             return 0;
         }
     }
