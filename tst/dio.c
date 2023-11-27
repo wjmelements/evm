afa0b7ee9be1f975262dcef2f4cb3392ddde662e
William Morriss <wjmelements@gmail.com>
2023-11-27 17:33:51 -0600

    tst dio construct

--- tst/dio.c
+++ tst/dio.c
@@ -109,9 +109,44 @@ void test_applyConfig_balance() {
     evmFinalize();
 }
 
+void test_applyConfig_construct() {
+    evmInit();
+
+    const char config[] = "["
+        "{"
+            "\"address\":\"0x80d9b122dc3a16fdc41f96cf010ffe7e38d227c3\","
+            "\"construct\":\"tst/in/quine.evm\","
+            "\"code\":\"0x383d3d39383df3\""
+        "}"
+    "]";
+    dioInit("bin/evm");
+    applyConfig(config);
+
+    uint64_t gas = 0x521b;
+    address_t from;
+    val_t val;
+    val[0] = 0;
+    val[1] = 0;
+    val[2] = 0;
+    address_t to = AddressFromHex42("0x80d9b122dc3a16fdc41f96cf010ffe7e38d227c3");
+    data_t input;
+    input.size = 0;
+
+    result_t result = txCall(from, gas, to, val, input);
+    op_t expected[] = {
+        0x38, 0x3d, 0x3d, 0x39, 0x38, 0x3d, 0xf3
+    };
+    assert(result.returnData.size == sizeof(expected));
+    assert(memcmp(expected, result.returnData.content, result.returnData.size) == 0);
+    assert(result.gasRemaining == 0);
+
+    evmFinalize();
+}
+
 int main() {
     test_applyConfig_code();
     test_applyConfig_storage();
     test_applyConfig_balance();
+    test_applyConfig_construct();
     return 0;
 }
