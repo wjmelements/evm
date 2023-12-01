d607e0415863816703b195b3b3b01ac96118c496
William Morriss <wjmelements@gmail.com>
2023-11-30 18:12:48 -0600

    fprintLabels with -j

--- include/scan.h
+++ include/scan.h
@@ -1,6 +1,9 @@
 #include "ops.h"
+#include <stdio.h>
 
 void scanInit();
 op_t scanNextOp(const char **iter);
 int scanValid(const char **iter);
 void scanFinalize(op_t *begin, uint32_t *programLength);
+
+void fprintLabels(FILE *);
