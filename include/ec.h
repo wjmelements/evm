0571f834f5b982d92f5f751bb42560c96e8be39e
William Morriss <wjmelements@gmail.com>
2024-11-06 16:12:42 -0600

    secp256k1

--- include/ec.h
+++ include/ec.h
@@ -2,4 +2,4 @@
 #include "uint256.h"
 
 
-address_t ec_recover(unsigned char v, uint256_t r, uint256_t s);
+address_t ec_recover(uint256_t msg_hash, unsigned char v, uint256_t r, uint256_t s);
--- src/ec.c
+++ src/ec.c
@@ -1,8 +1,62 @@
 #include "ec.h"
 #include "keccak.h"
 
+// ECC: yy = xxx+ax+b
 
-address_t ec_recover(unsigned char v, uint256_t r, uint256_t s) {
+// secp256k1
+// A = 0, B = 7
+// The order aka N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+static uint256_t SECP256K1_A = {
+    {
+        {
+            {
+                0,
+                0
+            }
+        },
+        {
+            {
+                0,
+                0
+            }
+        }
+    }
+};
+static uint256_t SECP256K1_B = {
+    {
+        {
+            {
+                0,
+                0
+            }
+        },
+        {
+            {
+                0,
+                7
+            }
+        }
+    }
+};
+static uint256_t SECP256K1_N = {
+    {
+        {
+            {
+                0xffffffffffffffff,
+                0xfffffffffffffffe
+            }
+        },
+        {
+            {
+                0xbaaedce6af48a03b,
+                0xbfd25e8cd0364141
+            }
+        }
+    }
+};
+
+
+address_t ec_recover(uint256_t msg_hash, unsigned char v, uint256_t r, uint256_t s) {
     // TODO
     address_t undefined;
     return undefined;
