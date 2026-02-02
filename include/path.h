0f6d225f6f2bb6f19cf7dd4c42f454012da1eee1
William Morriss <wjmelements@gmail.com>
2026-02-01 23:30:12 -0600

    simplify constructPath

--- include/path.h
+++ include/path.h
@@ -1,8 +1,5 @@
 void pathInit(const char *selfPath);
 
-// absolute path to currently-running evm
-const char *derivePath();
-
 // exec evm with currently-running evm
 data_t assemblePath(const char *path);
 
