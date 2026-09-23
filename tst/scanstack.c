99bf77da12a3b8f35354f30c775c5da120b63961
William Morriss <wjmelements@gmail.com>
2026-09-23 18:03:12 -0500

    fix: remove test of undefined function

--- tst/scanstack.c
+++ tst/scanstack.c
@@ -8,17 +8,5 @@ int main() {
     assert(!scanstackEmpty());
     assert(DUP3 == scanstackPop());
     assert(scanstackEmpty());
-
-    data_t data;
-    data.size = 2;
-    data.content = malloc(2);
-    data.content[0] = STOP;
-    data.content[1] = PUSH0;
-    scanstackPushData(&data);
-    assert(!scanstackEmpty());
-    assert(STOP == scanstackPop());
-    assert(!scanstackEmpty());
-    assert(PUSH0 == scanstackPop());
-    assert(scanstackEmpty());
     return 0;
 }
